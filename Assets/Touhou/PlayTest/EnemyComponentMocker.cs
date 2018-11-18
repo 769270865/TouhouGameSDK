621493a163671907f73a1354f7ebff1d0ade658d
769270865 <wly769270865@hotmail.com>
2018-11-17 21:37:04 -0800

    Update to Unity 2018.2.16

---
+++ Assets/Touhou/PlayTest/EnemyComponentMocker.cs
@@ -0,0 +1,61 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using Touhou.Core.Enemy;
+using UnityEngine;
+
+
+namespace Touhou.PlayTest.EnemyUtlities
+{
+    public class EnemyComponentMocker : SerializedMonoBehaviour
+    {
+        [OdinSerialize]
+        public IBulletPattern TestBulletPattern;
+        [OdinSerialize]
+        public ISpellCard TestSpellCard;
+        public float TestEnemyComponentLifeTime;
+        public bool isDefeatedMock;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine("TestingProcess");
+        }
+
+        IEnumerator TestingProcess()
+        {
+            if (isDefeatedMock)
+            {
+                yield return new WaitForSecondsRealtime(TestEnemyComponentLifeTime / 2 + Random.Range(+0.5f, -0.5f));
+                if (TestBulletPattern != null)
+                {
+                    TestBulletPattern.Health.DecreseHealth(100);
+                }
+                else if (TestSpellCard != null)
+                {
+                    TestSpellCard.Health.DecreseHealth(100);
+                }
+
+            }
+            else
+            {
+                yield return new WaitForSecondsRealtime(TestEnemyComponentLifeTime / 2 + Random.Range(+0.5f, -0.5f));
+                if (TestBulletPattern != null)
+                {
+                    TestBulletPattern.Health.DecreseHealth(50);
+                }
+                else if (TestSpellCard != null)
+                {
+                    TestSpellCard.Health.DecreseHealth(50);
+                }
+            }
+        }
+
+
+
+    }
+}
+
+
